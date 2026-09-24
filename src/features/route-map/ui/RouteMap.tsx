2d08b2410dc74c6f2be5240046b2a10b9a44b483
Timofey Mukhin <muhintv@gmail.com>
2026-09-24 10:40:41 +0200

    configured Leaflet marker assets

--- src/features/route-map/ui/RouteMap.tsx
+++ src/features/route-map/ui/RouteMap.tsx
@@ -9,10 +9,19 @@ import {
 import styles from './RouteMap.module.css';
 import { useEffect } from 'react';
 import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import type {
     RouteBuildGeometry,
     RouteStop,
 } from '../../../entities/route';
+
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+});
 
 
 interface RouteMapProps {
